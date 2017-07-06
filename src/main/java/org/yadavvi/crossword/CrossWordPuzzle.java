9b3fe359867a825a20cd10f823ace6d1bd9ab71d
Vishal Yadav <yadavvi91@gmail.com>
2017-07-07 00:57:03 +0530

    Using the permutation to check the validity of outputs generated.

--- src/main/java/org/yadavvi/crossword/CrossWordPuzzle.java
+++ src/main/java/org/yadavvi/crossword/CrossWordPuzzle.java
@@ -35,28 +35,6 @@ public class CrossWordPuzzle {
         return returnValue;
     }
 
-    public char[][] solveCrossWordPuzzle(char[][] input, List<String> cities) {
-        List<Places> places = getPlaces(input);
-        Collections.sort(places);
-        Collections.sort(cities, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.length() - o2.length();
-            }
-        });
-
-        List<List<Integer>> combinations = generateCombinations(places);
-        System.out.println(combinations);
-
-        /*List<Places> visitedPlaces = new LinkedList<>();
-        List<List<Places>> validWays = new LinkedList<>();
-        solveCrossWordPuzzle(input, cities, places, visitedPlaces, validWays);
-        for (List<Places> validWay : validWays) {
-            System.out.println(validWay);
-        }*/
-        return input;
-    }
-
     private static List<List<Integer>> generateCombinations(List<Places> places) {
         List<List<Integer>> combinations = new LinkedList<>();
         combinations.add(new LinkedList<>());
@@ -93,6 +71,136 @@ public class CrossWordPuzzle {
         return combinations;
     }
 
+    public char[][] solveCrossWordPuzzle(char[][] input, List<String> cities) {
+        List<Places> places = getPlaces(input);
+        Collections.sort(places);
+        Collections.sort(cities, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+
+        List<List<Integer>> combinations = generateCombinations(places);
+        System.out.println(combinations);
+
+        return solveCrossword(combinations, input, places, cities);
+    }
+
+    private char[][] solveCrossword(List<List<Integer>> combinations, char[][] input, List<Places> places,
+                                    List<String> cities) {
+        char[][] workingInput = createNewArray(input);
+        List<Integer> tillNow = new LinkedList<>();
+        List<Integer> breakValues = new LinkedList<>();
+
+        for (List<Integer> combination : combinations) {
+            tillNow.clear(); // Clear the combinations browsed till now.
+            copyArray(input, workingInput);
+
+            if (areFirstElementsSame(breakValues, combination)) continue;
+            else breakValues.clear();
+
+            for (Integer comb : combination) {
+                if (isCombinationValid(comb, places, cities, workingInput)) {
+                    tillNow.add(comb);
+                } else {
+                    breakValues.addAll(tillNow);
+                    breakValues.add(comb);
+                    break;
+                }
+            }
+            if (tillNow.size() == combination.size()) return workingInput;
+        }
+
+        return null;
+    }
+
+    private void copyArray(char[][] source, char[][] destination) {
+        for (int i = 0; i < source.length; i++) {
+            for (int j = 0; j < source[i].length; j++) {
+                destination[i][j] = source[i][j];
+            }
+        }
+    }
+
+    private boolean areFirstElementsSame(List<Integer> breakValues, List<Integer> combination) {
+        if (breakValues.isEmpty()) return false;
+
+        for (int i = 0; i < breakValues.size(); i++) {
+            if (!breakValues.get(i).equals(combination.get(i))) return false;
+        }
+
+        return true;
+    }
+
+    private boolean isCombinationValid(Integer comb, List<Places> places,
+                                       List<String> cities, char[][] input) {
+        Places place = places.get(comb);
+        String city = cities.get(comb);
+        if (place instanceof Horizontal) {
+            int i = ((Horizontal) place).start;
+            int row = ((Horizontal) place).row;
+            for (char charValue : city.toCharArray()) {
+                if (input[row][i] != '-') {
+                    if (input[row][i] == charValue) {
+                        i++;
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+                input[((Horizontal) place).row][i++] = charValue;
+            }
+        } else if (place instanceof Vertical) {
+            int i = ((Vertical) place).start;
+            int column = ((Vertical) place).column;
+            for (char charValue : city.toCharArray()) {
+                if (input[i][column] != '-') {
+                    if (input[i][column] == charValue) {
+                        i++;
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+                input[i++][column] = charValue;
+            }
+        }
+        return true;
+    }
+
+    private char[][] getOutputArray(char[][] input, String city, Places place, char[][] output) {
+        if (place instanceof Horizontal) {
+            int i = ((Horizontal) place).start;
+            int row = ((Horizontal) place).row;
+            for (char charValue : city.toCharArray()) {
+                if (output[row][i] != '-') {
+                    if (output[row][i] == charValue) {
+                        continue;
+                    } else {
+                        return null;
+                    }
+                }
+                output[((Horizontal) place).row][i++] = charValue;
+            }
+        } else if (place instanceof Vertical) {
+            int i = ((Vertical) place).start;
+            int column = ((Vertical) place).column;
+            for (char charValue : city.toCharArray()) {
+                if (output[i][column] != '-') {
+                    if (output[i][column] == charValue) {
+                        continue;
+                    } else {
+                        return null;
+                    }
+                }
+                output[i++][column] = charValue;
+            }
+        }
+
+        return output;
+    }
+
     private void solveCrossWordPuzzle(char[][] input, List<String> cities, List<Places> places,
                                       List<Places> visitedPlaces, List<List<Places>> validWays) {
         if (places.isEmpty()) {
@@ -123,7 +231,7 @@ public class CrossWordPuzzle {
     }
 
     private char[][] getNewInputArray(char[][] input, String city, Places place) {
-        char[][] newInput = copyOldArray(input);
+        char[][] newInput = createNewArray(input);
         if (place instanceof Horizontal) {
             int i = ((Horizontal) place).start;
             int row = ((Horizontal) place).row;
@@ -155,7 +263,7 @@ public class CrossWordPuzzle {
         return newInput;
     }
 
-    private char[][] copyOldArray(char[][] input) {
+    private char[][] createNewArray(char[][] input) {
         char[][] newInput = new char[input.length][];
         for (int i = 0; i < input.length; i++) {
             newInput[i] = new char[input[i].length];

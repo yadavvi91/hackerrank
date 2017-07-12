6ecdd25691e67a5aee47afa82383581ee3075585
Vishal Yadav <yadavvi91@gmail.com>
2017-07-12 20:52:23 +0530

    Added new test cases that were failing.

--- src/test/java/org/yadavvi/backtracking/KFactorizationTest.java
+++ src/test/java/org/yadavvi/backtracking/KFactorizationTest.java
@@ -20,6 +20,10 @@ public class KFactorizationTest {
                 new Object[]{12, new int[]{2, 3, 4}, new int[]{1, 3, 12}},
                 new Object[]{15, new int[]{2, 10, 6, 9, 11}, new int[]{-1}},
                 new Object[]{72, new int[]{2, 4, 6, 9, 3, 7, 16, 10, 5}, new int[]{1, 2, 8, 72}},
+                // Input-Output 3
+                new Object[]{610360465, new int[]{9, 12, 15}, new int[]{-1}},
+                // Input-Output 4
+                new Object[]{964518043, new int[]{4}, new int[]{-1}},
                 // Input-Output 11
                 new Object[]{231000000, new int[]{2, 3, 5, 7, 11, 13, 17, 19},
                         new int[]{1, 2, 4, 8, 16, 32, 64, 192, 960, 4800, 24000, 120000, 600000, 3000000, 21000000, 231000000}},
